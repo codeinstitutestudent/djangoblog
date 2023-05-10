96a62871f6922d8b46593488785894377ee9ff48
codeinstitutestudent <testciproject@gmail.com>
2023-05-10 16:15:56 +0000

    add about generic view

--- about/views.py
+++ about/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import About
 # Create your views here.
+
+
+class AboutMe(ListView):
+    model = About
